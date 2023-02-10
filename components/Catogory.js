6070fe282f8e43b357e3eab94e60782cebf96624
Divya Raj <divyaraj7612@gmail.com>
2023-02-10 20:18:28 +0530

    navigation bar created

---
+++ components/Catogory.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Catogory = () => {
+  return (
+    <View>
+      <Text>Catogory</Text>
+    </View>
+  )
+}
+
+export default Catogory
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,31 +1,61 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Image } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
-import { useLayoutEffect } from 'react'
+import { SafeAreaView,View, Text, TextInput, ScrollView } from "react-native";
+import React from "react";
+import { Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { useLayoutEffect } from "react";
+import { AdjustmentsVerticalIcon, ChevronDownIcon, UserIcon} from "react-native-heroicons/outline";
+
 
 const HomeScreen = () => {
-    const navigation = useNavigation();
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerShown: false
-        })
-
-    })
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  });
 
   return (
+    <View className="bg-white">
+      {/* NavigationBar */}
     <View className="h-20 w-screen items-end flex-row">
-        <View>
-            <Image
-            source={{
-                uri: "https://tse3.mm.bing.net/th?id=OIP.DvOvIJ-XTfMgFKreCxzM7gHaE7&pid=Api&P=0",
-            }}
-            className="w-12 h-12 bg-white rounded-full mx-2"
-            />
-        </View>
-      <Text className="text-red-500">HomeScreen</Text>
+      <View>
+        <Image
+          source={{
+            uri:
+              "https://tse3.mm.bing.net/th?id=OIP.DvOvIJ-XTfMgFKreCxzM7gHaE7&pid=Api&P=0",
+          }}
+          className="w-12 h-12 bg-white rounded-full mx-2 px-4"
+        />
+      </View>
+      <View className="flex-1">
+        <Text className="font-bold text-gray-400 text-s">Deliver Here</Text>
+        <Text className="font-bold text-xl">
+          Current Location
+          <ChevronDownIcon color="#00CCBB" />
+        </Text>
+      </View>
+      <UserIcon size={35} color="#00CCBB" />
     </View>
-  )
-}
+   
+    {/* Seachbar */}
+    <View className="flex-row h-12 items-center my-2" >
+        <View className="flex-1 mx-3  bg-gray-100">
+          {/* <SearchIcon/> */}
+          <TextInput placeholder="Search for Restaurents" className="h-12" />
+        </View>
+        <AdjustmentsVerticalIcon
+        size={30}
+        color="#00CCBB"
+        />
+      </View>
 
-export default HomeScreen
+{/* body */}
+<ScrollView>
+  
+</ScrollView>
+
+      </View>
+  );
+};
+
+export default HomeScreen;
